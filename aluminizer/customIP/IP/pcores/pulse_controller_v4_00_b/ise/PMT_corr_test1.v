0e1be17287b36b16ed7139afa09b9f52fb7ee27d
Yichao Yu <yyc1992@gmail.com>
2015-03-04 10:17:06 -0500

    [misc] whitespace, indent

--- aluminizer/customIP/IP/pcores/pulse_controller_v4_00_b/ise/PMT_corr_test1.v
+++ aluminizer/customIP/IP/pcores/pulse_controller_v4_00_b/ise/PMT_corr_test1.v
@@ -24,54 +24,51 @@
 
 module test1;
 
-	// Inputs
-	reg clk;
-	reg reset;
-	reg pulse_in;
-	reg sync_in;
+   // Inputs
+   reg clk;
+   reg reset;
+   reg pulse_in;
+   reg sync_in;
 
-	// Outputs
-	wire [127:0] data_out;
-	wire dataready_out;
+   // Outputs
+   wire [127:0] data_out;
+   wire dataready_out;
 
-	// Instantiate the Unit Under Test (UUT)
-	PMT_correlation2 uut (
-		.clk(clk), 
-		.reset(reset), 
-		.data_out(data_out), 
-		.dataready_out(dataready_out), 
-		.pulse_in(pulse_in), 
-		.sync_in(sync_in)
-	);
+   // Instantiate the Unit Under Test (UUT)
+   PMT_correlation2 uut (.clk(clk),
+                         .reset(reset),
+                         .data_out(data_out),
+                         .dataready_out(dataready_out),
+                         .pulse_in(pulse_in),
+                         .sync_in(sync_in));
 
-	initial begin
-		// Initialize Inputs
-		clk = 0;
-		reset = 1;
-		pulse_in = 0;
-		sync_in = 0;
+   initial begin
+      // Initialize Inputs
+      clk = 0;
+      reset = 1;
+      pulse_in = 0;
+      sync_in = 0;
 
-		// Wait 50 ns for global reset to finish
-		#50;
-        
-		// Add stimulus here
-    reset = 0;
-	end
-      
-  // Clock generator
-  always begin
-       #5  clk = ~clk; // Toggle clock every 5 ticks
-  end
-  
-  always begin
-       #71  pulse_in = 0;
-       #71  pulse_in = 1;
-  end
-  
+      // Wait 50 ns for global reset to finish
+      #50;
+
+      // Add stimulus here
+      reset = 0;
+   end
+
+   // Clock generator
    always begin
-       #35    sync_in = 0;
-       #71    sync_in = 1;
-		 #35    sync_in = 0;
-  end
-endmodule
+      #5  clk = ~clk; // Toggle clock every 5 ticks
+   end
 
+   always begin
+      #71  pulse_in = 0;
+      #71  pulse_in = 1;
+   end
+
+   always begin
+      #35    sync_in = 0;
+      #71    sync_in = 1;
+      #35    sync_in = 0;
+   end
+endmodule
