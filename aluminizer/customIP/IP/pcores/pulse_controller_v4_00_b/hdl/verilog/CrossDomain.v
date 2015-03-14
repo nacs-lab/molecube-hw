ad269d935acbcd5b52b369122e240feefdee1029
Yichao Yu <yyc1992@gmail.com>
2015-03-14 10:41:11 -0400

    [misc] verilog 2001, format

--- aluminizer/customIP/IP/pcores/pulse_controller_v4_00_b/hdl/verilog/CrossDomain.v
+++ aluminizer/customIP/IP/pcores/pulse_controller_v4_00_b/hdl/verilog/CrossDomain.v
@@ -1,16 +1,15 @@
 /* Cross clock domains.
-
- Copied from here, with small modifications:
- http://www.fpga4fun.com/CrossClockDomain2.html
-
- "The trick is to transform the flags into level changes,
- and then use the two flip-flops technique."
-
+ *
+ * Copied from here, with small modifications:
+ * http://www.fpga4fun.com/CrossClockDomain2.html
+ *
+ * "The trick is to transform the flags into level changes,
+ * and then use the two flip-flops technique."
  */
 
 module CrossDomain(input InA, input clkB, output OutB);
    // this changes level on rising edges of InA
-   reg Toggle;
+   reg Toggle = 0;
    always @(posedge InA) Toggle <= Toggle ^ InA;
 
    // which can then be sync-ed to clkB
@@ -19,9 +18,4 @@ module CrossDomain(input InA, input clkB, output OutB);
 
    // and recreate the flag in clkB
    assign OutB = (SyncA_clkB[2] ^ SyncA_clkB[1]);
-
-   // initialize for simulation
-   initial begin
-      Toggle = 0;
-   end
 endmodule
