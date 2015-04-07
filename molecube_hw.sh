cb9e663561cfc81fb85404d3b23f7d6eeedbf7ec
Yichao Yu <yyc1992@gmail.com>
2015-04-07 00:47:12 -0400

    [tcl] runner

--- molecube_hw.sh
+++ molecube_hw.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
 base_dir=$(dirname "$(realpath "${BASH_SOURCE}")")
-bin_dir=$(pwd)
 
-vivado -mode batch -source /dev/stdin <<EOF
-set base_dir "$base_dir"
-set bin_dir "$bin_dir"
-source "$base_dir/molecube_hw.tcl"
-EOF
+exec "$base_dir"/run_vivado_tcl "$base_dir/molecube_hw.tcl"
