8c250071230be8c7216a50ca68893e5f7323a6d4
Yichao Yu <yyc1992@gmail.com>
2015-04-07 11:08:20 -0400

    [tcl] move scripts

--- molecube_hw.sh
+++ molecube_hw.sh
@@ -2,4 +2,4 @@
 
 base_dir=$(dirname "$(realpath "${BASH_SOURCE}")")
 
-exec "$base_dir"/run_vivado_tcl "$base_dir/molecube_hw.tcl"
+exec "$base_dir"/run_vivado_tcl "$base_dir/scripts/molecube_hw.tcl"
