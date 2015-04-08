f92a1cd8042a8c13856ccd18eebf0830951ea6d8
Yichao Yu <yyc1992@gmail.com>
2015-04-08 13:55:38 -0400

    [design] move

--- scripts/molecube_hw.tcl
+++ scripts/molecube_hw.tcl
@@ -10,7 +10,7 @@ set src_set [ensure_fileset -srcset sources_1]
 set_property "ip_repo_paths" \
     "[file normalize "$bin_dir/custom_ip/pulse_controller_5.0"]" $src_set
 
-source "$base_dir/design_1.tcl"
+source "$base_dir/scripts/design_1.tcl"
 set design_dir "$bin_dir/molecube_hw/molecube_hw.srcs/sources_1/bd/design_1"
 set design_file "$design_dir/design_1.bd"
 
