ffb4c30fcc374abdfad8084d6884a1666530973c
Yichao Yu <yyc1992@gmail.com>
2020-05-04 16:11:36 -0400

    [script] Add check and reset on runs
    * Do not return if the run is up to date
    * Reset the run before running otherwise a previous run can cause error

--- script/build.tcl
+++ script/build.tcl
@@ -27,8 +27,22 @@ puts [format "Compiling project %s to %s with %d processes" $proj $bitfile $jobn
 
 open_project $proj
 update_compile_order -fileset sources_1
-launch_runs impl_1 -jobs $jobn
-wait_on_run impl_1
+set synth [get_runs synth_1]
+if {[get_property NEEDS_REFRESH $synth] ||
+    [get_property PROGRESS $synth] ne "100%"
+} then {
+    reset_run synth_1
+    launch_runs synth_1 -jobs $jobn
+    wait_on_run synth_1
+}
+set impl [get_runs impl_1]
+if {[get_property NEEDS_REFRESH $impl] ||
+    [get_property PROGRESS $impl] ne "100%"
+} then {
+    reset_run impl_1
+    launch_runs impl_1 -jobs $jobn
+    wait_on_run impl_1
+}
 open_run impl_1
 set path [file dirname $bitfile]
 file mkdir $path
