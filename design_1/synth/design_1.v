f8acf9ab87b617a06549ec134dde0e8f5b6648c3
Yichao Yu <yyc1992@gmail.com>
2020-05-04 20:21:58 -0400

    [design_1] include generated IP files

--- design_1/synth/design_1.v
+++ design_1/synth/design_1.v
@@ -1,7 +1,7 @@
 //Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 //--------------------------------------------------------------------------------
 //Tool Version: Vivado v.2019.2 (lin64) Build 2708876 Wed Nov  6 21:39:14 MST 2019
-//Date        : Sun May  3 13:37:17 2020
+//Date        : Mon May  4 16:39:11 2020
 //Host        : yyc.yyc-arch.org running 64-bit Arch Linux
 //Command     : generate_target design_1.bd
 //Design      : design_1
