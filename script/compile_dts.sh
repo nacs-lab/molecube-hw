88853b5ac4bdf88bafd2575892f41da469c39e84
Yichao Yu <yyc1992@gmail.com>
2021-09-10 11:45:10 -0400

    [script] Patch the pulse controller version

--- script/compile_dts.sh
+++ script/compile_dts.sh
@@ -5,5 +5,9 @@ out=$2
 
 indir=$(dirname "$in")
 
+# Patch the pulse controller version to remove the minor version
+# We'll bump the major version when the compatibility with the kernel driver is broken.
+
 gcc -I "$indir" -E -nostdinc -undef -D__DTS__ -x assembler-with-cpp -o - "$in" | \
+    sed -e 's/\(xlnx,pulse-controller-[0-9]*\)\.[0-9]*/\1/' | \
     dtc -I dts -O dtb -o "$out" -
