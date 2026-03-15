31c56fe677da3dbc65e540d897a6c81853b85a44
Yichao Yu <yyc1992@gmail.com>
2026-03-15 09:21:54 -0400

    wiring out ttl inputs

--- custom_ip/pulse_controller/xgui/pulse_controller_v5_2.tcl
+++ custom_ip/pulse_controller/xgui/pulse_controller_v5_2.tcl
@@ -9,6 +9,7 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "C_S00_AXI_HIGHADDR" -parent ${Page_0}
 
   ipgui::add_param $IPINST -name "U_PULSE_WIDTH"
+  ipgui::add_param $IPINST -name "U_TTL_IN_WIDTH"
 
 }
 
@@ -63,6 +64,15 @@ proc update_PARAM_VALUE.U_PULSE_WIDTH { PARAM_VALUE.U_PULSE_WIDTH } {
 
 proc validate_PARAM_VALUE.U_PULSE_WIDTH { PARAM_VALUE.U_PULSE_WIDTH } {
 	# Procedure called to validate U_PULSE_WIDTH
+	return true
+}
+
+proc update_PARAM_VALUE.U_TTL_IN_WIDTH { PARAM_VALUE.U_TTL_IN_WIDTH } {
+	# Procedure called to update U_TTL_IN_WIDTH when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.U_TTL_IN_WIDTH { PARAM_VALUE.U_TTL_IN_WIDTH } {
+	# Procedure called to validate U_TTL_IN_WIDTH
 	return true
 }
 
@@ -141,5 +151,10 @@ proc update_MODELPARAM_VALUE.N_DDS { MODELPARAM_VALUE.N_DDS PARAM_VALUE.N_DDS } 
 proc update_MODELPARAM_VALUE.N_SPI { MODELPARAM_VALUE.N_SPI PARAM_VALUE.N_SPI } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.N_SPI}] ${MODELPARAM_VALUE.N_SPI}
+}
+
+proc update_MODELPARAM_VALUE.U_TTL_IN_WIDTH { MODELPARAM_VALUE.U_TTL_IN_WIDTH PARAM_VALUE.U_TTL_IN_WIDTH } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.U_TTL_IN_WIDTH}] ${MODELPARAM_VALUE.U_TTL_IN_WIDTH}
 }
 
